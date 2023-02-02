3dc62f7e0ff7da9ee6437f90eca5cbedb1068075
LewinsHp <lewinsanhamonanganpanjaitan@gmail.com>
2023-02-02 16:36:17 +0700

    firstcommit

---
+++ src/data/BlogData.js
@@ -0,0 +1,71 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Website BSWV2",
+        tags:["Laravel"],
+        date:"16 Dec, 2021",
+        imgSrc:"https://regional.co.id/property/uploads/2022/03/iStock-968819844-1024x666-1.jpg",
+        link:"https://batamport.bpbatam.go.id/"
+    },
+    {
+        id:2,
+        name:"NETFLIX CLONE",
+        tags:["SASS"],
+        date:"19 Dec, 2021",
+        imgSrc:"https://assets.nflxext.com/ffe/siteui/vlv3/862cc171-8df5-418c-886f-2aaf767ae159/d46ac4b6-e5e4-406b-9bd6-24527044a0d9/ID-en-20230130-popsignuptwoweeks-perspective_alpha_website_large.jpg",
+        link:"https://www.netflix.com/id-en/"
+    },
+    {
+        id:3,
+        name:"Portofolio",
+        tags:["Html","Css","Javascript"],
+        date:"6 July, 2021",
+        imgSrc:"https://atlas-content-cdn.pixelsquid.com/stock-images/cartoon-boy-resting-7G5QWB4-600.jpg",
+        link:"https://lewinp.netlify.app/"
+    },
+    {
+        id:4,
+        name:"clone music",
+        tags:["react"],
+        date:"26 januari, 2023",
+        imgSrc:"https://i.ytimg.com/vi/FB1YNEOspyA/maxresdefault.jpg",
+        link:"https://soundcloud.com/"
+    },
+    {
+        id:5,
+        name:"Games Marbadai",
+        tags:["HTML, SASS"],
+        date:"3 Feb, 2021",
+        imgSrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa_fgXFer_lS-Iv7Po56cVK5RZyIi8LMnbRw&usqp=CAU",
+        link:"marbadai.netlify.app/"
+    },
+    {
+        id:6,
+        name:"Games I See You",
+        tags:["Html","css","Js"],
+        date:"5 Jan, 2021",
+        imgSrc:"https://cdn.akamai.steamstatic.com/steam/apps/1850740/capsule_616x353.jpg?t=1674129979",
+        link:"https://i-see-you.vercel.app/t"
+
+    },
+
+    {
+        id:7,
+        name:"Animation Space Sederhana",
+        tags:["HTML","CSS"],
+        date:"16 May, 2021",
+        imgSrc:"https://onepagelove.imgix.net/2017/05/opl-big-46.jpg?w=1200&fit=crop&h=628&crop=focalpoint&fp-y=0&auto=compress&blur=400&mark64=aHR0cHM6Ly9vbmVwYWdlbG92ZS5pbWdpeC5uZXQvMjAxNy8wNS9vcGwtYmlnLTQ2LmpwZz93PTk2MCZmaXQ9Y3JvcCZoPTYyOCZjcm9wPWZvY2FscG9pbnQmZnAtej0xJmZwLXk9MCZib3JkZXI9NCwwRkZGJmJvcmRlci1yYWRpdXM9NCw0LDQsNCZib3JkZXItcmFkaXVzLWlubmVyPTQsNCw0LDQmZm09cG5n&mark-align=center&exp=-6&mark-y=120",
+        link:"https://animation-space.vercel.app/"
+    },
+    {
+        id:8,
+        name:"Single Sign On",
+        tags:["Laravel","Jquery","Css","Bootstrap"],
+        date:"26 Januari, 2023",
+        imgSrc:"https://frontegg.com/wp-content/uploads/2021/12/SSO-Authentication-Agile-1.svg",
+        link:"https://sso.com"
+    },
+
+]
---
+++ src/data/WorkData.js
@@ -0,0 +1,32 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Customer Service",
+        description:"PT Adira Multi Finance",
+        tags:["2 Month"],
+
+        demo:"https://www.adira.co.id/",
+        github:"https://www.adira.co.id/"
+    },
+    {
+        id:2,
+        name:"Frontend Developer",
+        description:"PT Manggala Teknologi Nusantara",
+        tags:["1 Month"],
+
+        demo:"https://www.manggalateknologi.com/",
+        github:"https://www.manggalateknologi.com/"
+
+    },
+    {
+        id:3,
+        name:"Software Engineering",
+        description:"PT Aero Systems Indonesia",
+        tags:["1 Month"],
+
+        demo:"https://www.asyst.co.id/",
+        github:"https://www.asyst.co.id/"
+        },
+]
