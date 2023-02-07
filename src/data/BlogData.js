8abd7e0810e28d575d41fbaa12666a2cfd58c45c
LewinsHp <lewinsanhamonanganpanjaitan@gmail.com>
2023-02-07 11:43:43 +0700

    blogdata

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -5,54 +5,47 @@ export const Blogs = [
         id:1,
         name:"BSWV2",
         tags:["LARAVEL"],
-        date:"16 Des, 2021",
+        date:"16 Des, 2022",
         imgSrc:"https://regional.co.id/property/uploads/2022/03/iStock-968819844-1024x666-1.jpg",
         link:"https://batamport.bpbatam.go.id/"
     },
     {
         id:2,
         name:"NETFLIX CLONE",
-        tags:["SASS"],
-        date:"19 Des, 2021",
+        tags:["JS","SCSS"],
+        date:"19 Des, 2022",
         imgSrc:"https://assets.nflxext.com/ffe/siteui/vlv3/862cc171-8df5-418c-886f-2aaf767ae159/d46ac4b6-e5e4-406b-9bd6-24527044a0d9/ID-en-20230130-popsignuptwoweeks-perspective_alpha_website_large.jpg",
-        link:"https://www.netflix.com/id-en/"
+        link:"https://netflix-clone-06.netlify.app/"
     },
     {
         id:3,
-        name:"PORTO",
+        name:"PORTOFOLIO DESIGN",
         tags:["REACT JS","CSS","JS"],
-        date:"6 Jul, 2021",
+        date:"03 Jan, 2023",
         imgSrc:"https://d1ebn1hssciovn.cloudfront.net/prod/wp-content/uploads/2017/04/15170854/rsz_02.jpg",
         link:"https://lewinp.netlify.app/"
     },
+
     {
         id:4,
-        name:"CLONE SOUNDCLOUD",
-        tags:["REACT JS"],
-        date:"26 Jan, 2023",
-        imgSrc:"https://i.ytimg.com/vi/FB1YNEOspyA/maxresdefault.jpg",
-        link:"https://soundcloud.com/"
-    },
-    {
-        id:5,
         name:"GAMES",
         tags:["HTML, SASS","JQUERY"],
-        date:"3 Feb, 2021",
+        date:"03 Feb, 2022",
         imgSrc:"https://meliorgames.com/wp-content/uploads/2019/11/3-d-games.jpg",
         link:"https://marbadai.netlify.app/"
     },
     {
-        id:6,
+        id:5,
         name:"GAMES I SEE YOU",
         tags:["HTML","CSS","JS"],
-        date:"5 Jan, 2021",
+        date:"05 Jan, 2021",
         imgSrc:"https://cdn.akamai.steamstatic.com/steam/apps/1850740/capsule_616x353.jpg?t=1674129979",
-        link:"https://i-see-you.vercel.app/t"
+        link:"https://i-see-you.vercel.app/"
 
     },
 
     {
-        id:7,
+        id:6,
         name:"ANIMATION SPACE",
         tags:["HTML","CSS","JS"],
         date:"16 May, 2021",
@@ -60,28 +53,19 @@ export const Blogs = [
         link:"https://animation-space.vercel.app/"
     },
     {
-        id:8,
+        id:7,
         name:"SSO",
         tags:["LARAVEL","JQUERY","CSS","BOOTSTRAP"],
-        date:"26 Jan, 2023",
+        date:"20 Sep, 2022",
         imgSrc:"https://frontegg.com/wp-content/uploads/2021/12/SSO-Authentication-Agile-1.svg",
-        link:"https://sso.com"
+        link:"https://sso.batam.go.id"
     },
     {
-        id:9,
+        id:8,
         name:"REDESIGN WEBSITE HKBP",
         tags:["HTML","CSS","JS"],
-        date:"16 Jan, 2019",
-        imgSrc:"https://rotuawebsite.com/wp-content/uploads/2021/07/thumb-portfolio-hkbp-petukangan.jpg",
-        link:"https://hkbp.or.id/"
+        date:"01 Jan, 2023",
+        imgSrc:"https://hkbpcipayungcilangkap.org/asset/img_novapage/images/gereja1.JPG",
+        link:"https://www.hkbprawamangun.id/"
     },
-    {
-        id:10,
-        name:"UI DESIGN FUTSAL APP",
-        tags:["FIGMA"],
-        date:"26 Sept, 2020",
-        imgSrc:"https://kirantiwari.com.np/wp-content/uploads/2021/12/Dribbble-6-min-800x800.png",
-        link:"https://www.gosport.co.id/futsal/"
-    },
-
 ]
